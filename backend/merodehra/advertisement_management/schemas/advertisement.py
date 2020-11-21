9612f57a63d02fa47bc1ddeb46d4ec8ed9420b5e
bishwas76 <45958112+bishwas76@users.noreply.github.com>
2020-11-21 10:29:47 +0545

    multiple image upload

--- backend/merodehra/advertisement_management/schemas/advertisement.py
+++ backend/merodehra/advertisement_management/schemas/advertisement.py
@@ -1,5 +1,5 @@
 from ma import ma
-from advertisement_management.models.advertisement import AdvertisementModel
+from advertisement_management.models.advertisement import AdvertisementModel, ImageModel
 
 
 class AdvertisementSchema(ma.SQLAlchemyAutoSchema):
@@ -13,3 +13,10 @@ class AdvertisementSchema(ma.SQLAlchemyAutoSchema):
 class SearchAdvertisementSchema(ma.Schema):
     class Meta:
         fields = ("location_to_search",)
+
+
+class ImageSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ImageModel
+        dump_only = ("advertisement_id",)
+        include_fk = True
